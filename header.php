ab5f618a824934388525087b6c14c5cbe3f2e644
Leo Suarez <Leo@DTRaleigh.com>
2017-11-26 14:21:21 -0500

    Rolled back JQuery.

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
 <html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-<head>
-<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/jquery.min.js"></script>
+<head profile="http://gmpg.org/xfn/11">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
@@ -16,34 +18,38 @@
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Cantata+One" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Cantata+One' rel='stylesheet' type='text/css'>
 
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-<script>
-	$(function() {
-		var pull 		= $('#pull');
-			menu 		= $('nav ul');
-			menuHeight	= menu.height();
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>
+		$(function() {
+			var pull 		= $('#pull');
+				menu 		= $('nav ul');
+				menuHeight	= menu.height();
 
-		$(pull).on('click', function(e) {
-			e.preventDefault();
-			menu.slideToggle();
-		});
+			$(pull).on('click', function(e) {
+				e.preventDefault();
+				menu.slideToggle();
+			});
 
-		$(window).resize(function(){
-			var w = $(window).width();
-			if(w > 320 && menu.is(':hidden')) {
-				menu.removeAttr('style');
-			}
+			$(window).resize(function(){
+        		var w = $(window).width();
+        		if(w > 320 && menu.is(':hidden')) {
+        			menu.removeAttr('style');
+        		}
+    		});
 		});
-	});
-</script>
+	</script>
+
+
 
 <?php wp_head(); ?>
-
 </head>
+
+
 <body>
 <div id="header">
 	<h1 class="theTitle"><a href="https://dtraleigh.com/">The Raleigh Connoisseur</a></h1>
