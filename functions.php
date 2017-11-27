f850cb2b493b8e9621b189df7aa66e47f8dcac73
Leo Suarez <Leo@DTRaleigh.com>
2017-11-26 19:52:10 -0500

    Created the rss_append custom field so that I can add a custom message at the end of the RSS.

--- functions.php
+++ functions.php
@@ -1,4 +1,27 @@
 <?php //Begin widget code
-if ( function_exists('register_sidebars') )
-    register_sidebars(3);
+	if ( function_exists('register_sidebars') )
+		register_sidebars(3);
+	// End widget code
+
+	// Begin RSS Custom Field
+	function postrss($content) {
+		global $wp_query;
+
+		$postid = $wp_query->post->ID;
+		$rss_append = get_post_meta($postid, 'rss_append', true);
+
+		if(is_feed()) {
+			if($rss_append !== '') {
+				$content = $content . "<br /><br /><div>" . $rss_append . "</div>";
+			}
+			else {
+				$content = $content;
+			}
+		}
+		return $content;
+	}
+	
+	add_filter('the_excerpt_rss', 'postrss');
+	add_filter('the_content', 'postrss');
+	// End RSS Customer Field
 ?>
